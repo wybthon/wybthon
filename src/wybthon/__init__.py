ae279f35220fc3e2563492ad8b24a0f43076265d
Owen Carey <owenthcarey@gmail.com>
2025-10-12 14:29:11 -0700

    chore(pyproject,package): bump version to 0.6.0

--- src/wybthon/__init__.py
+++ src/wybthon/__init__.py
@@ -19,7 +19,7 @@ from .forms import (
 )
 from .reactivity import batch, computed, effect, on_effect_cleanup, signal, use_resource
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 # Detect Pyodide/browser environment where `js` module exists
 _IN_BROWSER = False
